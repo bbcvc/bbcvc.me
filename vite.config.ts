6cd290b157cfdb6732bf7a785e1e81700a2ff920
bbcvc <1103787680@qq.com>
2022-06-04 09:16:01 +0800

    chore；PWA 8

--- vite.config.ts
+++ vite.config.ts
@@ -144,7 +144,33 @@ const config: UserConfig = {
     }),
 
     // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({}),
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      manifest: {
+        name: 'Name of your app',
+        short_name: 'Short name of your app',
+        description: 'Description of your app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    }),
 
     Inspect(),
 
