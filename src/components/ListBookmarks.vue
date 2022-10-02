81bb4959b3d1ce31a2b147411e9c1988916d62f1
邢成龙 <chenglong.xing@cyclone-robotics.com>
2022-10-02 12:03:31 +0800

    chore: update

--- src/components/ListBookmarks.vue
+++ src/components/ListBookmarks.vue
@@ -1,5 +1,45 @@
 <script setup lang="ts">
 import bookmarks from '~/modules/bookmarks.json'
+const originName = (homeURL: string) => {
+  if (homeURL.includes('github'))
+    return 'GitHub'
+  if (homeURL.includes('notion'))
+    return 'Notion'
+  if (homeURL.includes('twitter'))
+    return 'Twitter'
+  if (homeURL.includes('youtube'))
+    return 'YouTube'
+  if (homeURL.includes('zhihu'))
+    return '知乎'
+  if (homeURL.includes('weibo'))
+    return '微博'
+  if (homeURL.includes('juejin'))
+    return '掘金'
+  if (homeURL.includes('segmentfault'))
+    return 'SegmentFault'
+  if (homeURL.includes('csdn'))
+    return 'CSDN'
+  if (homeURL.includes('bilibili'))
+    return 'Bilibili'
+  if (homeURL.includes('douyu'))
+    return '斗鱼'
+  if (homeURL.includes('zcool'))
+    return '站酷'
+  if (homeURL.includes('dribbble'))
+    return 'Dribbble'
+  if (homeURL.includes('codepen'))
+    return 'CodePen'
+  if (homeURL.includes('jsfiddle'))
+    return 'JSFiddle'
+  if (homeURL.includes('codepen'))
+    return 'CodePen'
+  if (homeURL.includes('stackoverflow'))
+    return 'Stack Overflow'
+  if (homeURL.includes('weixin'))
+    return 'wechart'
+
+  return homeURL
+}
 </script>
 
 <template>
@@ -10,16 +50,16 @@ import bookmarks from '~/modules/bookmarks.json'
       </div>
     </template>
     <app-link
-      v-for="route in bookmarks" :key="route.path"
+      v-for="route in bookmarks.filter(item => !!item.targetURL)" :key="route.targetURL"
       class="item block font-normal mb-6 mt-2 no-underline"
-      :to="route.path"
+      :to="route.targetURL"
     >
       <li class="no-underline">
         <div class="title text-lg">
           {{ route.title }}
         </div>
         <div class="time opacity-50 text-sm -mt-1">
-          <span v-if="route.origin" class="opacity-50">· {{ route.origin }}</span>
+          <span v-if="route.homeURL" class="opacity-50">· {{ originName(route.homeURL) }}</span>
         </div>
       </li>
     </app-link>
